90439304a24acc564a03edfd9a34ac8bfb765c86
Lin kouk <lindxa638@gmail.com>
2025-06-07 19:35:07 +0700

    add page

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      dd
+    <div>
+      
     </div>
   )
 }
